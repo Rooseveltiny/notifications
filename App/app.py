189d9e86479963335cb6a999fa61f04573e673fc
Rooseveltiny <mister.savvvik@gmail.com>
2020-02-28 09:46:56 +0300

    added secrep key checking

--- App/app.py
+++ App/app.py
@@ -9,7 +9,7 @@ sys.path.append(file_dir)
 from flask import Flask, render_template, request, redirect, escape, session, copy_current_request_context, jsonify
 
 app = Flask(__name__, template_folder='templates')
-app.secret_key = 'thisismysupperkey'
+app.secret_key = 'perokngsronsortv[troijvgdoitjbpoijb[otidj5u8-58he85-whghg049h-w49b-3tq-bw9-t98-39g-98t'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+file_dir+'/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -49,26 +49,30 @@ def notification_page(order_number):
 @app.route('/get_notifications', methods=["POST"])
 def get_notifications():
 
-    request_data = request.get_json(force=True)
-    order = Order(
-    uuid = request_data['uuid'], 
-    client_name = request_data['client_name'], 
-    order_number = request_data["order_number"],
-    status_of_order = request_data["status_of_order"],
-    date_of_getting = request_data["date_of_getting"]
-    )   
-    order.save()
-    
-    WarehousesStatus.delete_previous_data(request_data['uuid']) ### to refresh all statuses in all warehouses
-    for warehouse in request_data['warehouses_status']:
-        warehousestatus = WarehousesStatus(
-        order_uuid = request_data["uuid"],
-        warehouse_number = warehouse["warehouse"],
-        status = warehouse["status"]
-        )
-        warehousestatus.save()
+    if request.headers['secret-key'] == app.secret_key:
 
-    return 'ok'
+        request_data = request.get_json(force=True)
+        order = Order(
+        uuid = request_data['uuid'], 
+        client_name = request_data['client_name'], 
+        order_number = request_data["order_number"],
+        status_of_order = request_data["status_of_order"],
+        date_of_getting = request_data["date_of_getting"]
+        )   
+        order.save()
+        
+        WarehousesStatus.delete_previous_data(request_data['uuid']) ### to refresh all statuses in all warehouses
+        for warehouse in request_data['warehouses_status']:
+            warehousestatus = WarehousesStatus(
+            order_uuid = request_data["uuid"],
+            warehouse_number = warehouse["warehouse"],
+            status = warehouse["status"]
+            )
+            warehousestatus.save()
+
+        return 'ok'
+
+    return 'not ok)'
     
 @app.route("/db_create")
 def db_create_all():
